0c1db467a9510c8ca4b58aecacc0b2415ceb14ea
Soumya Ranjan Tripathy <srtsoumya21@gmail.com>
2021-05-19 21:33:39 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include <iostream>
+#include "redEyeReduction.h"
+#include "timer.h"
+
+#define PROFILE 1
+#define TONEMAPPING 1
+#define SEAMCARVING 0
+#define REDEYEREDUCTION 0
+#define BLUR 0
+#define CPU 0
+#define GPU 1
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	#if PROFILE
+		timerGPU.Start();
+	#endif
+		im = redEyeReduction::gpuRER(im, im2);
+	#if PROFILE
+		timerGPU.Stop();
+		printf("Total GPU time: %f msecs.\n", timerGPU.Elapsed());
+	#endif
+
+	imshow("Output",im);
+	
+	waitKey(0);
+
+	return 0;
+}
---
+++ redEyeReduction.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <cuda.h>
+#include <vector>
+#include "cuda_runtime.h"
+#include <iostream>
+#include <algorithm>
+#include "opencv2/highgui/highgui.hpp"
+
+using namespace std;
+using namespace cv;
+
+namespace redEyeReduction {
+	Mat cpuRER(Mat im, Mat eyeTemplate);
+	Mat gpuRER(Mat im, Mat eyeTemplate);
+}
